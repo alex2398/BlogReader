cc0bcea87000cf97c412add014589ba121139ca2
Alex Valladares <alexvalladares@Alexs-Mac.local>
2015-08-04 14:29:24 +0200

    Initial Commit

---
+++ BlogReader2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogReader2
+//
+//  Created by Alex Valladares on 04/08/15.
+//  Copyright (c) 2015 Alex Valladares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlogReader2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BlogReader2
+//
+//  Created by Alex Valladares on 04/08/15.
+//  Copyright (c) 2015 Alex Valladares. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
